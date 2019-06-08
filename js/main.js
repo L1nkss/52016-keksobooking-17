b37db9c6c50ac52f9bc39adbc2a47b53e968cc8b
L1nks <l1nks@inbox.ru>
2019-06-09 00:29:03 +0500

    jsdoc added

--- js/main.js
+++ js/main.js
@@ -1,30 +1,61 @@
 'use strict';
+
 var fragment = document.createDocumentFragment();
 var pinList = document.querySelector('.map__pins');
 var adTemplate = document.querySelector('#pin').content.querySelector('.map__pin');
+/**
+ * @constant {number} AD_COUNT - количество объявлений
+ */
 var AD_COUNT = 8;
-// размер pin'a
+/**
+ * @constant {number} PIN_WIDTH - ширина пина
+ * @constant {number} PIN_HEIGHT - высота пина
+ */
 var PIN_WIDTH = 50;
 var PIN_HEIGHT = 70;
 
 document.querySelector('.map').classList.remove('map--faded');
 
+/**
+ * Получить случайное число
+ *
+ * @param {number} min - начальное число
+ * @param {number} max - конечное число
+ * @return {number}
+ *
+ * @example
+ * getRandomInt(5, 15) // returns 9
+ */
 window.getRandomInt = function (min, max) {
   return Math.floor(Math.random() * (max - min)) + min;
 };
 
-// получить случайное объявление
+/**
+ * Генератор случайного объявления
+ *
+ * @return {object}
+ *
+ * @example
+ *
+ * {
+ *  author: { avatar: 'img/avatars/user02.png'},
+ *  offer: { type: 'flat'},
+ *  location: { x: 500, y: 320}
+ * }
+ *
+ */
 var getRandomAd = function () {
-  // получаем ширину карты где будет метка.
   var mapWidth = document.querySelector('.map__pins').offsetWidth;
-  // количество изображений, начиная с 1
-  var imageCount = 8;
-  // тип жилья
+  var IMAGE_COUNT = 8;
   var types = ['palace', 'flat', 'house', 'bungalo'];
-
-  // получаем случайное число и добавляем к нему 0
-  // формируем строку для аватара
-  var imageNumber = window.getRandomInt(1, imageCount);
+  /**
+   * @description Генерируем строку для пути к картинке
+   *
+   * @example
+   *
+   * imageString = 'img/avatars/user02.png'
+   */
+  var imageNumber = window.getRandomInt(1, IMAGE_COUNT);
   var imageString = 'img/avatars/user0' + imageNumber + '.png';
 
   return {
@@ -41,7 +72,12 @@ var getRandomAd = function () {
   };
 };
 
-// генерируем массив объявлений
+/**
+ * Получить массив случайных элементов
+ *
+ * @param {number} count -количество объявлений
+ * @return {array}
+ */
 var getRandomAds = function (count) {
   var ads = [];
   for (var i = 0; i < count; i++) {
@@ -50,8 +86,12 @@ var getRandomAds = function (count) {
 
   return ads;
 };
-
-// отрисовываем все объявления
+/**
+ * Рендерим Объявления
+ *
+ * @param { array } ads - массив объявлений
+ *
+ */
 var renderAds = function (ads) {
   for (var i = 0; i < ads.length; i++) {
     var x = ads[i].location.x - PIN_WIDTH / 2;
@@ -63,11 +103,14 @@ var renderAds = function (ads) {
     adElement.style = 'left: ' + x + 'px; top: ' + y + 'px;';
     adElement.querySelector('img').src = avatar;
     adElement.querySelector('img').alt = type;
-
-    // добавляем элемент во фрагмент
+    /**
+      * @description добавляем элемент в фрагмент
+    */
     fragment.appendChild(adElement);
   }
-  // отрисовываем всё
+  /**
+    * @description отрисовываем все элементы
+  */
   pinList.appendChild(fragment);
 };
 
